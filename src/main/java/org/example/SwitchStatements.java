c67d1f96a95599a6908a3e2e97bc9c723f206274
Ramya <ramyamutyam0098@gmail.com>
2025-12-01 10:29:22 +0530

    Conditionals and Loops

---
+++ src/main/java/org/example/SwitchStatements.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class SwitchStatements {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+    }
+}
